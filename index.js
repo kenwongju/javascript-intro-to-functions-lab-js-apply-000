53f4cf1990aef5bbd931a0308f5c36109769e98c
Kenneth Wong <kenwongjune@gmail.com>
2018-08-02 01:10:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function shout (string) {return string.toUpperCase()}
 function whisper (string) {return string.toLowerCase()}
 function logShout (string) {return console.log(string.toUpperCase())}
 function logWhisper (string) {return console.log(string.toLowerCase())}
+<<<<<<< HEAD
 function sayHiToGrandma(string) {if (string === string.toLowerCase()) return "I can't hear you!";
 else if (string === string.toUpperCase()) return "YES INDEED!";
 else if (string === "I love you, Grandma.")
 return "I love you, too.";}
+=======
+function sayHiToGrandma(string) {return "I can't hear you!" string.toLowerCase()}
+>>>>>>> 88f99816df9e524332342af9278ffb5fa0a77c70
